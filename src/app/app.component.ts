313218a4e28154e78b0f9f0e5527465f06f9cdf7
Drumshtick <ekodnar@outlook.com>
2022-02-03 11:52:55 -0600

    added comments and tested referrencing class properties with this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,20 @@ interface Country {
 @Component({
   selector: 'butt-stuff',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  styles: [`.f-awesome {background-color: black}`]// Directly change styles or link a stylesheet
+  styleUrls: ['./app.component.scss'],// To link to a stylesheet
+  styles: [`.f-awesome {background-color: black}`]// Directly change styles
 })
 export class AppComponent {
-  title = 'test';
+  // Properties
+  // [ public | private | protected ]
+  // Private does not help with security issues! Only private at compile-time
+  // Signifies that the variable is only to be used within the class
+  // Protected is like private but shows that it is used by derived class-
+  // Public signifies that the class method or property will be used outside the class
+  title: string = 'test';
   faCoffee = faCoffee;
-  COOKIES="COOKIE MONSTER LIKE COOKIES 🍲🍲🍲🍲";
+  COOKY: string="COOKIES 🍲🍲🍲🍲";
+  COOKIES: string = `COOKIE MONSTA NEED ${this.COOKY}`
   countries: Country[] = [
     {
       name: 'Russia',
@@ -38,4 +45,8 @@ export class AppComponent {
       population: 324459463
     },
   ];
+  // Invoked when the class is first instantiated
+  public constructor() {
+
+  }
 }
