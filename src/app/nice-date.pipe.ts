8069f84a1c3aadfa96b2f62776a6e6a680655047
Drumshtick <ekodnar@outlook.com>
2022-02-03 20:43:23 -0600

    my first custom pipe :tada:

---
+++ src/app/nice-date.pipe.ts
@@ -0,0 +1,47 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'niceDate'
+})
+export class NiceDatePipe implements PipeTransform {
+
+  transform(value: Date): string {
+    const CurrentDay: Date = new Date();
+    const change: number = Math.floor((
+      Date.parse(value.toString()) -
+      Date.parse(CurrentDay.toString())) / 86400000);
+
+      if (change === -1) {
+        return 'yesterday';
+      }
+
+      if (change === 0) {
+        return 'today';
+      }
+
+      if (change === 1) {
+        return "tomorrow";
+      }
+
+      const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+
+      const dayOfWeek: number = CurrentDay.getDay();
+
+      const givenDayofWeek: number  = value.getDay();
+
+      if ( (givenDayofWeek > dayOfWeek) && change < 7) {
+        return days[givenDayofWeek];
+      }
+
+      return value.toDateString();
+  }
+
+}
